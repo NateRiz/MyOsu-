6374ba66bac8ccde9e83c9d268b836c577681fea
NateRiz <riziknat@msu.edu>
2017-10-26 13:16:34 -0400

    Init commit

---
+++ MyOsu!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOsu!
+//
+//  Created by Robert Wei on 10/26/17.
+//  Copyright © 2017 Nathan Rizik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
