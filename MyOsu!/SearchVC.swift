fa065c18e06f9483f9c19275771db8ba7190f72e
NateRiz <riziknat@msu.edu>
2017-10-26 15:07:34 -0400

    Search UI

---
+++ MyOsu!/SearchVC.swift
@@ -0,0 +1,44 @@
+//
+//  SearchVC.swift
+//  MyOsu!
+//
+//  Created by Robert Wei on 10/26/17.
+//  Copyright © 2017 Nathan Rizik. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController{//}, UISearchBarDelegate{
+
+    
+    @IBOutlet weak var OsuSearchBar: UIStackView!
+    @IBOutlet weak var SearchResultStack: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //OsuSearchBar.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //print(searchBar.text)
+    //}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
