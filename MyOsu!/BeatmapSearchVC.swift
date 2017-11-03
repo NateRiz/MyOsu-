89f3e7e5025d2b4288c9c0f9ea0b3348dcc1475f
NateRiz <riziknat@msu.edu>
2017-11-02 20:18:11 -0400

    song name, artist and duration all in table in BM search view

--- MyOsu!/BeatmapSearchVC.swift
+++ MyOsu!/BeatmapSearchVC.swift
@@ -9,20 +9,63 @@
 import UIKit
 import SwiftSoup
 
-class BeatmapSearchVC: UIViewController {
+class SongSearchCell: UITableViewCell{
+    @IBOutlet weak var SongImage: UIImageView!
+    @IBOutlet weak var SongName: UILabel!
+    @IBOutlet weak var SongArtist: UILabel!
+    @IBOutlet weak var SongDuration: UILabel!
+}
+
+struct Song{
+    var name = ""
+    var artist = ""
+    var duration = ""
+    var id = ""
+    var image = UIImage()
+    
+    init(name:String = "", artist:String = "", duration:String = "", id:String = "")
+    {
+        self.name = name
+        self.artist = artist
+        self.duration = duration
+        self.id = id
+    }
+}
+
+class BeatmapSearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    
     
     var query = String()
     var statuses = [String]()
+    var SearchResults = [Song]()
+    @IBOutlet weak var SongTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.scrapeSearch()
-        // Do any additional setup after loading the view.
+        SongTable.delegate = self
+        SongTable.dataSource = self
+        SongTable.rowHeight = 100
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.SearchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: SongSearchCell = self.SongTable.dequeueReusableCell(withIdentifier: "songSearchCell") as! SongSearchCell
+        cell.SongName.text = SearchResults[indexPath.row].name
+        cell.SongArtist.text = SearchResults[indexPath.row].artist
+        cell.SongDuration.text = SearchResults[indexPath.row].duration
+        cell.SongImage.image = SearchResults[indexPath.row].image
+        
+        
+        return cell
     }
     
     
@@ -44,6 +87,24 @@ class BeatmapSearchVC: UIViewController {
             for elem in elems.array()
             {
                 print(try elem.text())
+                let text:[String] = try elem.text().components(separatedBy: " ")
+                let id = text[0]
+                var name = ""
+                var itr = 1
+                while text[itr] != "-"{
+                    name+=text[itr] + " "
+                    itr+=1
+                }
+                itr+=1
+                var artist = ""
+                while (Array(text[itr])[0]) != "["{
+                    artist += text[itr] + " "
+                    itr+=1
+                }
+                let duration = text[itr]
+                self.SongTable.beginUpdates()
+                SearchResults.append(Song(name:name, artist:artist, duration:duration, id:id))
+                self.SongTable.endUpdates()
             }
         }
         catch let error {
@@ -51,6 +112,8 @@ class BeatmapSearchVC: UIViewController {
             print(error)
         }
     }
+    
+    
     
     
 }
